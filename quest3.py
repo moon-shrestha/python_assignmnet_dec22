74192aaa696bd6fc7b6ba5388ea2326c92094b85
moontoon <moon.shresth@gmail.com>
2017-12-26 11:03:46 +0545

    to capitalize every word

---
+++ quest3.py
@@ -0,0 +1,10 @@
+'''Write a program that accepts sequence of lines as input and prints the lines after making all characters
+in the sentence capitalized.'''
+
+name_str = "my name is moon shrestha."
+
+print(name_str)
+
+print("Capitalized string:")
+
+print(name_str.upper())

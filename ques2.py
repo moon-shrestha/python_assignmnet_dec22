579ca08356bdd6b2be9192b24bb35636ec11a529
moontoon <moon.shresth@gmail.com>
2017-12-26 10:58:29 +0545

    to sort in alphabetcal order

---
+++ ques2.py
@@ -0,0 +1,10 @@
+'''Write a program that accepts a comma separated sequence of words as input and prints the words in a
+comma-separated sequence after sorting them alphabetically.'''
+
+shoe_collection = ["nike", "puma", "addidas", "converse", "vans"]
+
+print(shoe_collection)
+
+print("Sorted in alphabetical order:")
+
+print(sorted(shoe_collection))
